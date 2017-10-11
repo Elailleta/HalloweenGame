c0b9c295944ccef7984169274dc113be33a91d60
Elailleta <jessica.stride@ocsbstudent.ca>
2017-10-11 09:51:08 -0400

    added and did some code for pickups

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
+    private int count;
+    public Text CountText;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        count = 0;
 	}
 	
 	// Update is called once per frame
@@ -20,5 +24,14 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
     }
 }
